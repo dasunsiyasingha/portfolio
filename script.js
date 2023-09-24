21f51f6719efed8af2039f2874eebca4086d551f
dasunsiyasingha <121418942+dasunsiyasingha@users.noreply.github.com>
2023-09-24 14:16:27 +0530

    Frist

---
+++ script.js
@@ -0,0 +1,36 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+function reveal_contain() {
+    var reveals = document.querySelectorAll(".reveal_container");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active1");
+      } else {
+        reveals[i].classList.remove("active1");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal_contain);
